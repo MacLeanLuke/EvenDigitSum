145d08b4177c38ee2c11d25f5a8881e2c45fbbc4
Luke MacLean <40969439+MacLeanLuke@users.noreply.github.com>
2021-09-22 21:50:10 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("the sum of the even digits in 123456789 is " + EvenDigitSum.getEvenDigitSum(123456789));
+        System.out.println("the sum of the even digits in 252 is " + EvenDigitSum.getEvenDigitSum(252));
+        System.out.println("the sum of the even digits in -22 is " + EvenDigitSum.getEvenDigitSum(-22));
+    }
+}
